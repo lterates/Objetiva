4d4f66a2b83364df96bdf1fb76d20eb0364dc9da
lterates <9180199@esmad.ipp.pt>
2021-01-29 05:03:01 +0000

    various fixes and such

--- app/src/main/java/com/example/objetiva/HomePage.java
+++ app/src/main/java/com/example/objetiva/HomePage.java
@@ -6,15 +6,27 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
 
     public class HomePage extends AppCompatActivity {
     boolean logged = false;
+    private RequestQueue mQueue;
     Fragment selectedFragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,28 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
         Button seePricingBtn = findViewById(R.id.seePricingBtn);
+
+        Button parseBtn = findViewById(R.id.parseBtn);
+        mQueue = Volley.newRequestQueue(this);
+    }
+
+    public void jsonParse(View view){
+        TextView responseTxt = findViewById(R.id.responseTxt);
+        String url = "https://trabalhos.esmad.ipp.pt/tsiw/20-21/nes/wp_group06/wp-json/wc/v3/products?consumer_key=ck_c9a44480dab7f33199745d06604ae5f8049b3729&consumer_secret=cs_4c1a6a42e1422af263dfd4177e8f8cb464b8eb9f";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("Response:", response.toString());
+                        responseTxt.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error", error.getMessage());
+            }
+        });
+        mQueue.add(request);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
---
+++ app/src/main/java/com/example/objetiva/SearchPage.java
@@ -0,0 +1,16 @@
+package com.example.objetiva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_page);
+
+
+    }
+}
